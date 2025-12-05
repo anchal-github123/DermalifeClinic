e8ab676c21ce839536362daf1739d4a750314cd5
anchal-github123 <anchalnayak2003@gmail.com>
2025-12-05 17:29:57 +0530

    changing image size and add name

--- Frontend/src/components/ClinicGallery.jsx
+++ Frontend/src/components/ClinicGallery.jsx
@@ -1,9 +1,5 @@
 import Gallery from "./Gallery";
-import img1 from "../assets/images/gallery1.webp";
-import img2 from "../assets/images/gallery2.webp";
-import img3 from "../assets/images/LipFilller.webp";
-import img4 from "../assets/images/carbonpeel.webp";
-import img5 from "../assets/images/Burn Scar Removal.webp";
+
 import img6 from "../assets/images/eyebro.webp";
 import img7 from "../assets/images/Facelift.webp";
 import img8 from "../assets/images/earlob.webp";
@@ -11,7 +7,17 @@ import img9 from "../assets/images/knees.webp";
 import img10 from "../assets/images/melasma.webp";
 import img11 from "../assets/images/botox.webp";
 import img12 from "../assets/images/Acne-scars (1).webp";
+
 export default function ClinicGallery() {
-  const images = [img1, img2, img3, img4, img5,img6,img7,img8,img9,img10,img11,img12];
+  const images = [
+    { src: img6, name: "Eyebrow Micropigmentation" },
+    { src: img9, name: "Dark Knees Treatment" },
+    { src: img10, name: "Melasma Treatment" },
+    { src: img8, name: "Ear Lobe Repair" },
+    { src: img11, name: "Botox Treatment" },
+    { src: img12, name: "Acne Scar Laser Treatment" },
+    { src: img7, name: "Facelift" }
+  ];
+
   return <Gallery images={images} />;
 }
--- Frontend/src/components/Gallery.jsx
+++ Frontend/src/components/Gallery.jsx
@@ -1,21 +1,25 @@
 export default function Gallery({ images }) {
   return (
-    <div className="py-12 bg-gray-100">
+    <div className="bg-gray-100 py-5">
       <h2 className="text-3xl font-bold text-center mb-10 text-gray-800">
         Our Clinic Gallery
       </h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 px-4 max-w-6xl mx-auto">
-        {images.map((img, index) => (
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3">
+        {images.map((item, index) => (
           <div
             key={index}
-            className="overflow-hidden rounded-xl shadow-md group cursor-pointer"
+            className="overflow-hidden rounded-xl shadow-md group cursor-pointer bg-white"
           >
             <img
-              src={img}
-              className="w-full h-48 object-cover group-hover:scale-110 duration-500"
-              alt="clinic"
-              />
+              src={item.src}
+              className="w-full h-45 object-cover group-hover:scale-110 duration-500"
+              alt={item.name}
+            />
+
+            <p className="text-center font-semibold text-gray-700 py-3 text-sm">
+              {item.name}
+            </p>
           </div>
         ))}
       </div>
