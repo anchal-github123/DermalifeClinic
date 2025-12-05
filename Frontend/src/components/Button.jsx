9e86bd266f5d873a945512ac012d6677b149c4cb
anchal-github123 <anchalnayak2003@gmail.com>
2025-12-05 17:40:55 +0530

    chnage of button effect

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -1,8 +1,11 @@
 export default function Button({ text, bgcolor, textcol, hover, type, link, onClick }) {
   const baseClass =
-    `flex items-center justify-between gap-2 cursor-pointer font-medium
-     text-[0.9rem] sm:text[0.8rem] md:text-[1.2rem] px-5 py-3 md:px-6 md:py-4 lg:px-8 lg:py-4 
-     rounded-full shadow-sm hover:shadow-md transition`;
+     `
+  flex items-center justify-between gap-2 cursor-pointer font-medium shadow:2xl 
+  text-[0.9rem] sm:text[0.8rem] md:text-[1.2rem] px-5 py-3 md:px-6 md:py-4 lg:px-8 lg:py-4 
+  rounded-full transition-all duration-300
+  hover:scale-105 hover:shadow-lg hover:-translate-y-1 border-1
+`;
 
   const style = {
     backgroundColor: `var(--color-${bgcolor})`,
